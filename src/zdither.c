a923ce51dcaf4b5fc33035faa0c13aa82c8e6c0d
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-08-28 10:31:52 -0400

    Clear incompatible-pointer-types warning.

--- src/zdither.c
+++ src/zdither.c
@@ -107,11 +107,11 @@ void ZB_ditherFrameBuffer(ZBuffer *zb,unsigned char *buf,
 {
   int xk,yk,x,y,c1,c2;
   unsigned char *dest1;
-  unsigned short *pp1;
+  PIXEL *pp1;
   int r_d,g_d,b_d;
   unsigned char *ctable=zb->dctable;
   register unsigned char *dest;
-  register unsigned short *pp;
+  register PIXEL *pp;
 
   assert( ((long)buf & 1) == 0 && (linesize & 1) == 0);
 
@@ -127,7 +127,7 @@ void ZB_ditherFrameBuffer(ZBuffer *zb,unsigned char *buf,
       r_d=((c1 << 2) & 0xF800) >> 2;
       g_d=(c1 >> 4) & 0x07C0;
       b_d=(c1 >> 9) & 0x001F;
-      
+
       r_d|=(((c2 << 2) & 0xF800) >> 2) << 16;
       g_d|=((c2 >> 4) & 0x07C0) << 16;
       b_d|=((c2 >> 9) & 0x001F) << 16;
@@ -135,7 +135,7 @@ void ZB_ditherFrameBuffer(ZBuffer *zb,unsigned char *buf,
 
       dest1=buf + (yk * linesize) + xk;
       pp1=zb->pbuf + (yk * zb->xsize) + xk;
-      
+
       for(y=yk;y<zb->ysize;y+=4) {
 	dest=dest1;
 	pp=pp1;
