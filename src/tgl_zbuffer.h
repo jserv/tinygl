2578d52fea8e847250c6cd72917cf5343d11a1d0
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-08-29 23:34:17 -0400

    Don't need prefixes on src .c files

--- src/tgl_zbuffer.h
+++ src/tgl_zbuffer.h
@@ -35,9 +35,9 @@
 
 // internal format:
 //#define TGL_FEATURE_RENDER_BITS    15
-#define TGL_FEATURE_RENDER_BITS    16
+//#define TGL_FEATURE_RENDER_BITS    16
 //#define TGL_FEATURE_RENDER_BITS    24
-//#define TGL_FEATURE_RENDER_BITS    32 
+#define TGL_FEATURE_RENDER_BITS    32 
 
 /*
  * Z buffer
