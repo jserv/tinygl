46f3a0c1c848b51fec01de515e6bb8114c4c7145
Jim Huang <jserv.tw@gmail.com>
2022-06-12 00:29:01 +0800

    Remove inlined hack

--- src/memory.c
+++ src/memory.c
@@ -1,17 +1,10 @@
 /*
  * Memory allocator for TinyGL
  */
+
 #include <stdlib.h>
-#include <string.h>
 #include "zgl.h"
 
-static inline void required_for_compilation_()
-{
-    return;
-}
-
-/* modify these functions so that they suit your needs */
-#include <string.h>
 void gl_free(void *p)
 {
     free(p);
